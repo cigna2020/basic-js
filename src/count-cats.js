7a00b000fad338977a10863b0f8013ca52619463
Alex <cigan@ua.fm>
2021-02-26 18:14:59 +0200

    feat: install the dependency and create a function to 'count cats'

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,13 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function countCats(matrix) {
+    let res = 0;
+    matrix.forEach((el, indx) => {
+        for (let i = 0; i <= el.length - 1; i++) {
+            if (el[i] === '^^') {
+                res++;
+            };
+        };
+    })
+    return res;
 };
