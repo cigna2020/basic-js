f8bd7d486c6328faff213b2dd444a462818c201f
Alex <cigan@ua.fm>
2021-02-27 11:16:22 +0200

    feat: create a fuction 'Carbon dating'

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,9 +1,13 @@
 const CustomError = require("../extensions/custom-error");
 
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+    if (typeof sampleActivity == "string" && Number(sampleActivity) > 0 && Number(sampleActivity) <= 15) {
+        var res = Math.log(MODERN_ACTIVITY / Number(sampleActivity)) / (0.693 / HALF_LIFE_PERIOD);
+    } else {
+        return false;
+    }
+    return Math.ceil(res);
 };
