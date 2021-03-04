db3929cc8809056451de3f3400e9b2c10cd6dc5e
Alex <cigan@ua.fm>
2021-03-04 15:58:56 +0200

    feat: add a solution of the task 'repeater'

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,11 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+    options.separator ? options.separator : options.separator = '+';
+    options.additionSeparator ? options.additionSeparator : options.additionSeparator = '|';
+    options.addition !== null ? options.addition : options.addition = 'null';
+
+    const additionArray = new Array(options.additionRepeatTimes).fill(options.addition).join(options.additionSeparator);
+    return new Array(options.repeatTimes).fill(str + additionArray).join(options.separator);
 };
-  
+
