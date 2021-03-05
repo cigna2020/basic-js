8a49efc3c8e604e9a7cda3f0c2439b31f2af5b69
Alex <cigan@ua.fm>
2021-03-05 13:58:29 +0200

    feat: add a solution of task 'vigenere-cipher'

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,14 +1,65 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }
-}
+    constructor(flag) {
+        if (flag === undefined) {
+            this.flag = true;
+        } else {
+            this.flag = false;
+        }
+    }
+
+    encrypt(mess, key) {
+        if (!mess || !key) {
+            return
+        } else {
+            key = key.repeat(Math.ceil(mess.length / key.length));
+        }
+
+        const alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        let result = '';
+        let counter = 0;
+        for (let i = 0; i < mess.length; i++) {
+            if (/^[a-zA-Z]{1}$/.test(mess[i])) {
+                result = result + alphabet.charAt((alphabet.indexOf(mess.charAt(i)) + alphabet.indexOf(key.charAt(i - counter))) % alphabet.length);
+            }
+            else {
+                result = result + mess[i];
+                counter++;
+            };
+
+        }
+
+        result = !this.flag ? [...result].reverse().join('').toUpperCase() : result.toUpperCase();
+
+        return result;
+    };
+
+    decrypt(mess, key) {
+
+        if (!mess || !key) {
+            throw new Error();
+        } else {
+            key = key.repeat(Math.ceil(mess.length / key.length));
+        }
+
+        const alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        let result = '';
+        let counter = 0;
+
+        for (let i = 0; i < mess.length; i++) {
+            if (/^[a-zA-Z]{1}$/.test(mess[i])) {
+                result = result + alphabet.charAt(((alphabet.indexOf(mess.charAt(i)) + alphabet.length) - alphabet.indexOf(key.charAt(i - counter))) % alphabet.length);
+            } else {
+                result = result + mess[i];
+                counter++;
+            };
+        }
+        result = !this.flag ? [...result].reverse().join('').toUpperCase() : result.toUpperCase();
+        return result;
+
+    }
+};
+
 
 module.exports = VigenereCipheringMachine;
